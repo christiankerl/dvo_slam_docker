7ccb9db2d43aa00245e46e034ac92b4bcdb22e6b
Christian Kerl <christian.kerl@in.tum.de>
2016-09-13 14:56:14 +0200

    updated build.sh to work with ros kinetic

--- scripts/build.sh
+++ scripts/build.sh
@@ -19,11 +19,11 @@ catkin_make
 source $WS/devel/setup.bash
 
 cd $WS_SRC 
-git clone -b jade-devel https://github.com/tum-vision/dvo_slam.git
+git clone -b $ROS_DISTRO-devel https://github.com/tum-vision/dvo_slam.git
 
 mkdir $WS_SRC/dvo_slam/dvo_benchmark/output/
 
 cd $WS
-apt-get update && rosdep update && rosdep install -y -i dvo_core dvo_ros dvo_slam dvo_benchmark
+apt-get update && rosdep update && rosdep install --as-root apt:false -y -i dvo_core dvo_ros dvo_slam dvo_benchmark
 
 G2O_ROOT=/opt/ros/$ROS_DISTRO/ catkin_make -DCMAKE_BUILD_TYPE=Release
